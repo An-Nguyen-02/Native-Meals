e83fa6d4df4385a936843726b4c72a7bcf059b5e
An Nguyen <an.nguyen@tuni.fi>
2022-02-09 11:32:47 +0200

    initially layout of app

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StatusBar as ExpoStatusBar} from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar } from 'react-native';
+const isAndroid = Platform.OS === 'android';
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.first_view}>
+          <Text>Search</Text>
+        </View>
+        <View style={styles.second_view}>
+          <Text>List</Text>
+        </View>
+      </SafeAreaView>
+      <ExpoStatusBar style='auto'/>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: isAndroid ? StatusBar.currentHeight:0
   },
+  first_view: {
+    justifyContent: "center",
+    backgroundColor: "green",
+    padding: 16
+  },
+  second_view:{
+    flex:1,
+    backgroundColor: "blue",
+    padding: 16
+  }
+
 });
