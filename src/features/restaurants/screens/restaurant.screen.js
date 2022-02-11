be50ae23b3c73b1002a252016d25b8f028fef2e7
An Nguyen <nhatan020902@gmail.com>
2022-02-11 16:10:07 +0200

    add loading screen

--- src/features/restaurants/screens/restaurant.screen.js
+++ src/features/restaurants/screens/restaurant.screen.js
@@ -5,15 +5,29 @@ import { RestaurantInfoCard } from '../components/restaurant-info-card.component
 import styled from 'styled-components';
 import { SafeArea } from '../../../components/safe-area.component';
 import { RestaurantsContext } from '../../../services/restaurants/restaurants.context';
+import { ActivityIndicator, Colors } from 'react-native-paper';
 const SearchView = styled(View)`
   justify-content: center;
   padding-bottom: ${(props) => props.theme.space[1]};
+`;
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
+const LoadingView = styled(View)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
 `;
 
 export const RestaurantsScreen = () => {
   const { isLoading, error, restaurants } = useContext(RestaurantsContext);
   return (
     <SafeArea>
+      {isLoading && (
+        <LoadingView>
+          <Loading size={50} animating={true} color={Colors.purple300} />
+        </LoadingView>
+      )}
       <SearchView>
         <Searchbar placeholder="Search" />
       </SearchView>
